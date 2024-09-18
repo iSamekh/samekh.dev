7945c99d2116385d06e58bc0e750b6936614e10c
Eric Gomes <ee-gomes@outlook.com>
2024-09-18 17:09:43 -0300

    Itens do Carousel
    Itens do Carousel implementado pelo python para receber as informações dinâmicamente do back-end.

--- app.py
+++ app.py
@@ -1,34 +1,30 @@
 from flask import Flask, render_template
-#from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 
-'''
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr(self):
-        return '<Task %r>' % self.id
-'''
+projects = [
+    {
+        'thumbnail': '../static/assets/genericThumbnail.png',
+        'title': 'Website Samekh.dev',
+        'description': '''Portfólio criado usando HTML, CSS e JavaScript com os projetos apresentados de forma dinâmica.
+                          No back-end, Flask e Python gerenciam o conteúdo permitindo a integração de dados de forma eficiente.
+                          O site está hospedado no Render, com o código-fonte controlado via GitHub, facilitando a manutenção e atualizações contínuas.''',
+    },
+    {
+        'image': '../static/assets/profilePicture.png',
+        'title': 'Projeto B',
+        'description': '''Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                          culpa qui officia deserunt mollit anim id est laborum.''',
+    }
+]
 
 @app.route('/')
 def index():
-    return render_template("index.html")
-
-@app.route('/about')
-def about():
-    return render_template('index.html',active_page='home')
-
-
-
-
+    return render_template("index.html", projects = projects)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,87 +1,81 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-// Seleciona todos os botões do menu
-const buttons = document.querySelectorAll('.headerButton');
 
-// Mapeamento dos botões com as respectivas divs
-const sectionMap = {
-    aboutButton: document.getElementById('About'),
-    projectsButton: document.getElementById('Projects'),
-    contactsButton: document.getElementById('Contacts')
-};
+    const buttons = document.querySelectorAll('.headerButton');
 
-// Função para ativar o botão correspondente à seção
-const activateButton = (id) => {
-    buttons.forEach(button => button.classList.remove('active')); // Remove a classe 'active' de todos os botões
-    const activeButton = document.getElementById(id); // Ativa o botão correspondente à seção visível
-    if (activeButton) {
-        activeButton.classList.add('active');
-    }
-};
+    // Mapeamento dos botões com as respectivas divs
+    const sectionMap = {
+        aboutButton: document.getElementById('About'),
+        projectsButton: document.getElementById('Projects'),
+        contactsButton: document.getElementById('Contacts')
+    };
 
-// Configurações do IntersectionObserver
-const observerOptions = {
-    root: null, // Usa a viewport como referência
-    rootMargin: '0px',
-    threshold: 0.6 // Define 60% da seção visível como critério para ativar
-};
+    // Função para ativar o botão correspondente à seção
+    const activateButton = (id) => {
+        buttons.forEach(button => button.classList.remove('active')); // Remove a classe 'active' de todos os botões
+        const activeButton = document.getElementById(id); // Ativa o botão correspondente à seção visível
+        if (activeButton) {
+            activeButton.classList.add('active');
+        }
+    };
 
-// Cria um IntersectionObserver
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            const sectionId = entry.target.id;
-            switch (sectionId) {
-                case 'About':
-                    activateButton('aboutButton');
-                    break;
-                case 'Projects':
-                    activateButton('projectsButton');
-                    break;
-                case 'Contacts':
-                    activateButton('contactsButton');
-                    break;
+    // Configurações do IntersectionObserver
+    const observerOptions = {
+        root: null, // Usa a viewport como referência
+        rootMargin: '0px',
+        threshold: 0.6 // Define 60% da seção visível como critério para ativar
+    };
+
+    // Cria um IntersectionObserver
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const sectionId = entry.target.id;
+                switch (sectionId) {
+                    case 'About':
+                        activateButton('aboutButton');
+                        break;
+                    case 'Projects':
+                        activateButton('projectsButton');
+                        break;
+                    case 'Contacts':
+                        activateButton('contactsButton');
+                        break;
+                }
             }
-        }
-    });
-}, observerOptions);
+        });
+    }, observerOptions);
 
-// Observa as seções
-Object.values(sectionMap).forEach(section => {
-    if (section) {
-        observer.observe(section);
-    }
-});
-
-// Adiciona um evento de clique a cada botão
-buttons.forEach(button => {
-    button.addEventListener('click', function () {
-        buttons.forEach(btn => btn.classList.remove('active')); // Remove a classe 'active' de todos os botões
-        this.classList.add('active'); // Adiciona a classe 'active' ao botão clicado
-
-        // Obter o id do botão clicado e rolar suavemente para a seção correspondente
-        const section = sectionMap[this.id];
+    // Observa as seções
+    Object.values(sectionMap).forEach(section => {
         if (section) {
-            section.scrollIntoView({ behavior: 'smooth' });
+            observer.observe(section);
         }
     });
-});
 
-// Define o botão "Sobre" como 'ativo' e rola suavemente até a div #About
-const aboutButton = document.getElementById('aboutButton');
-if (aboutButton) {
-    aboutButton.classList.add('active');
-}
-const aboutSection = document.getElementById('About');
-if (aboutSection) {
-    aboutSection.scrollIntoView({ behavior: 'smooth' });
-}
-});
+    // Adiciona um evento de clique a cada botão
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            buttons.forEach(btn => btn.classList.remove('active')); // Remove a classe 'active' de todos os botões
+            this.classList.add('active'); // Adiciona a classe 'active' ao botão clicado
 
+            // Obter o id do botão clicado e rolar suavemente para a seção correspondente
+            const section = sectionMap[this.id];
+            if (section) {
+                section.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
 
-//Carousel
+    // Define o botão "Sobre" como 'ativo' e rola suavemente até a div #About
+    const aboutButton = document.getElementById('aboutButton');
+    if (aboutButton) {
+        aboutButton.classList.add('active');
+    }
+    const aboutSection = document.getElementById('About');
+    if (aboutSection) {
+        aboutSection.scrollIntoView({ behavior: 'smooth' });
+    }
 
-
-document.addEventListener('DOMContentLoaded', () => {
     const carousel = document.querySelector('#projectsCarousel');
     const projects = document.querySelectorAll('.projetos');
     
